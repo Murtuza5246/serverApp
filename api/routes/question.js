c414bc0a0c85c56bc5a74d4c1114148af48eb401
Murtuza5246 <murtuza5246@gmail.com>
2020-06-27 15:11:48 +0530

    sdd

--- api/routes/question.js
+++ api/routes/question.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
-const checkAuth = require("../middleware/check-auth.js");
 const Question = require("../model/question");
 const User = require("../model/user");
 const app = express();
